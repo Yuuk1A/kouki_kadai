dd3b3d1dec440a0c5a4625a9638e68a0f795ee69
Yuuk1A <zm18390@josai.ac.jp>
2021-02-07 09:52:45 +0000

    252

--- ex6-5.js
+++ ex6-5.js
@@ -1,4 +1,4 @@
-var image = new Array;
+var image = new Array();
 image[0]=new Image();
 image[0].scr="image/hanayome.jpg";
 image[1]=new Image();
@@ -10,7 +10,7 @@ image[3].scr="image/sikimori.jpg";
 
 var cnt=0;
 function slidesw(){if(document.getElementById){document.slide.elements[0].disabled=true;
-                    document.getElementById("sd").scr=image[cnt].scr;cnt++;
+                    document.getElementById("sd").scr = image[cnt].scr;cnt++;
                     if(cnt<=3){var timer1=setTimeout("slidesw()",5000);}
                     else{cnt=0;
                         document.slide.elements[0].disabled=false;clearTimeout(timer1);}}}
