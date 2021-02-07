b024256bbe8e59a337c82431eb20627e295dc200
Yuuk1A <zm18390@josai.ac.jp>
2021-02-07 09:47:34 +0000

    fgh

--- ex6-5.js
+++ ex6-5.js
@@ -1,12 +1,12 @@
 var image = new Array;
 image[0]=new Image();
-image[0].scr="img/0.png";
+image[0].scr="image/hanayome.jpg";
 image[1]=new Image();
-image[1].scr="img/1.png";
+image[1].scr="image/kaguya.jpg";
 image[2]=new Image();
-image[2].scr="img/3.png";
+image[2].scr="image/gavudoro.jpg";
 image[3]=new Image();
-image[3].scr="img/4.png";
+image[3].scr="image/sikimori.jpg";
 
 var cnt=0;
 function slidesw(){if(document.getElementById){document.slide.elements[0].disabled=true;
