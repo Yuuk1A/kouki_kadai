ce0ab375790836333a5519a740e2f464bc2d04ba
Yuuk1A <zm18390@josai.ac.jp>
2021-02-08 11:58:28 +0000

    2月8日

--- ex6-5.js
+++ ex6-5.js
@@ -2,3 +2,7 @@ function imgchange1(){document.getElementById('sd').src = './image/hanayome.jpg'
 function imgchange2(){document.getElementById('sd').src = './image/kaguya.jpg';}
 function imgchange3(){document.getElementById('sd').src = './image/sikimori.jpg';}
 function imgchange4(){document.getElementById('sd').src = './image/gavudoro.jpg';}
+
+
+
+
